e1c6bf87e1da0b19a086c548ce693e97281adf61
text2 <team@text2.click>
2025-11-11 19:56:23 +0700

    a

--- js/data.js
+++ js/data.js
@@ -109,7 +109,7 @@ const partnersData = [
 const navigationData = [
   {
     name: "APP",
-    url: "https:/text2.click/app/",
+    url: "https://text2.click/app",
     dataLink: "APP",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
