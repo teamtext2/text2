f122ee813bd19e1b41e84bdbfa5cbede0e28b877
text2 <team@text2.click>
2025-11-14 16:10:21 +0700

    a

---
+++ noti/data.js
@@ -0,0 +1,11 @@
+// data.js
+const NOTIFICATIONS = [
+    {
+        title: "Notification - Click to view",
+        message: "We just launched our weather application"
+    },
+    {
+        title: "Notification - Click to view",
+        message: "Auto save is now available on the note app"
+    },
+];
