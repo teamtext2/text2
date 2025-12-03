ea84aba796b338ee0dda49d59876350f5c85c921
text2 <team@text2.click>
2025-12-03 23:06:26 +0700

    a

--- app/translate/sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-// Không viết sự kiện fetch để luôn load online
