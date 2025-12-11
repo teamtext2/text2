bc2f98fcedc53fd258e970736d8ed420ab0f6fab
text2 <team@text2.click>
2025-12-11 22:36:10 +0700

    ac

--- js/data.js
+++ js/data.js
@@ -62,13 +62,17 @@ const socialMediaData = [
     ]
   },
   {
-    platform: "Telegram",
-    name: "Telegram",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width:18px;height:18px;" fill="none">
-      <path d="M21.05 5.84c.13-.59-.23-.82-.77-.66l-16.07 5.22c-.58.18-.57.56-.1.71l4.11 1.28 1.57 5.08c.19.6.35.83.72.83.36 0 .52-.16.7-.54l2.01-4.09 4.24 3.88c.78.43 1.34.21 1.53-.72l2.07-9.99zm-3.85 2.05l-6.7 6.02-.24 2.49 1.78-3.62 5.16-4.89z" fill="currentColor"/>
+    platform: "post",
+    name: "Post",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width:18px;height:18px;">
+      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+      <polyline points="14,2 14,8 20,8"></polyline>
+      <line x1="16" y1="13" x2="8" y2="13"></line>
+      <line x1="16" y1="17" x2="8" y2="17"></line>
+      <polyline points="10,9 9,9 8,9"></polyline>
     </svg>`,
     links: [
-      { name: "HOA NHAT ANH", url: "https://t.me/hoanhatanh123/" }
+      { name: "text2 - post", url: "https://text2.medium.com" }
     ]
   }
 ];
