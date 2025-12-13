ea7f7f5219875b2ecd18134b88511384f79794f7
text2 <team@text2.click>
2025-12-14 00:51:58 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -92,7 +92,7 @@ const partnersData = [
 const navigationData = [
   {
     name: "APP",
-    url: "https://team.text02.com/",
+    url: "https://text02.com/app",
     dataLink: "GAME",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
