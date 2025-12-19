c33f6472d4a4dd48f7313e0402ecc5d9882c10c6
text2 <team@text2.click>
2025-12-20 00:21:02 +0700

    a

--- js/data.js
+++ js/data.js
@@ -112,9 +112,9 @@ const navigationData = [
     </svg>`
   },
   {
-    name: "Post",
-    url: "https://text02.com/post/",
-    dataLink: "https://text02.com/post/",
+    name: "TEAM",
+    url: "https://text02.com/team",
+    dataLink: "https://text02.com/team",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width:18px;height:18px;">
       <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
       <polyline points="14,2 14,8 20,8"></polyline>
