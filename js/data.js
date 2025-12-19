df767b37c29c99d6197c555ebd03511239187a1f
text2 <team@text2.click>
2025-12-20 00:04:51 +0700

    ac

--- js/data.js
+++ js/data.js
@@ -141,7 +141,7 @@ const sidebarData = {
     },
     {
       name: "Text2 Team",
-      url: "https://team.text02.com",
+      url: "https://text02.com/team",
       dataSameTab: true,
       icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width:18px;height:18px;">
         <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
